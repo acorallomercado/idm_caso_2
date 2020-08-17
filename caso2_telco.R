7f51d87f3020f650e6e503ca0df9f212b003b305
vgaudino <gaudinobello.vane@gmail.com>
2020-08-17 17:49:16 -0300

    Adelanto1

--- caso2_telco.R
+++ caso2_telco.R
@@ -5,12 +5,16 @@
 
 library(dbplyr)
 library(tidyverse)
+library(ggplot2)
 
 # Set working directory
 source("local_config.R")
 
 # Get data from file and create dataset
 data_raw <- read.csv("Churn.csv", sep=";", T)
+
+# Looking at the dataset
+str(data_raw)
 
 # Fix numeric values.
 dataset <- data_raw %>% mutate(Day_Mins = as.numeric(sub(',', '.', Day_Mins, fixed = TRUE))) %>%
@@ -38,8 +42,90 @@ table(dataset$Vmail_Plan)
 # Set categorical values as factor.
 dataset <- dataset %>% mutate(State = as.factor(State)) %>% 
   mutate(Area_Code = as.factor(Area_Code)) %>%
-  mutate(Phone = as.factor(Area_Code)) %>%
   mutate(Intl_Plan = as.factor(Intl_Plan)) %>%
   mutate(Vmail_Plan = as.factor(Vmail_Plan)) 
-  
+
+str(dataset)  
+summary(dataset)
+
+#Variables validation
+
+#Churn proportions
+sum.churn <- summary(dataset$Churn)
+sum.churn
+
+prop.churn <- sum(dataset$Churn == "TRUE") / length(dataset$Churn)
+prop.churn
+
+#Analasis de variables categoricas
+
+#Plan internacional
+counts1 <- table(dataset$Churn, dataset$Intl_Plan,
+                dnn=c("Churn", "International Plan"))
+sumtable1 <- addmargins(counts, FUN = sum)
+sumtable1
+
+#Proporcion por filas
+row.margin1 <- round(prop.table(counts1, margin = 1), 4)*100 
+row.margin1
+
+#Porporcion por columna
+col.margin1 <- round(prop.table(counts1, margin = 2), 4)*100
+col.margin1
+
+# Grafico de Barras Superpuesto Proporciones de churners por International Plan
+ggplot() +
+  geom_bar(data = dataset,
+           aes(x = factor(Intl_Plan),
+               fill = factor(Churn)),
+           position = "stack") +
+  scale_x_discrete("Customer Service Calls") +
+  scale_y_continuous("Percent") +
+  guides(fill=guide_legend(title="Churn")) +
+  scale_fill_manual(values=c("green", "violet"))
+
+#Plan buzón de voz 
+counts2 <- table(dataset$Churn, dataset$Vmail_Plan,
+                 dnn=c("Churn", "Voice Plan"))
+sumtable2 <- addmargins(counts2, FUN = sum)
+sumtable2
+
+#Proporcion por filas
+row.margin2 <- round(prop.table(counts2, margin = 1), 4)*100 
+row.margin2
+
+#Porporcion por columna
+col.margin2 <- round(prop.table(counts2, margin = 2), 4)*100
+col.margin2
+
+# Grafico de Barras Superpuesto Proporciones de churners por Plan de Voz 
+ggplot() +
+  geom_bar(data = dataset,
+           aes(x = factor(Vmail_Plan),
+               fill = factor(Churn)),
+           position = "stack") +
+  scale_x_discrete("Customer Service Calls") +
+  scale_y_continuous("Percent") +
+  guides(fill=guide_legend(title="Churn")) +
+  scale_fill_manual(values=c("green", "violet"))
+
+
+#Variables Nùmericas
+
+#Verificamos posible outliers
+ggplot(dataset, aes(x=Churn, y=CustServ_Calls)) + 
+  geom_boxplot()
+
+#Verficamos la cantidad de llamadas por 
+ggplot() +
+  geom_bar(data=dataset,
+           aes(x = factor(CustServ_Calls),
+               fill = factor(Churn)),
+           position = "fill") +
+  scale_x_discrete("Customer Service Calls") +
+  scale_y_continuous("Percent") +
+  guides(fill=guide_legend(title="Churn")) +
+  scale_fill_manual(values=c("green", "violet"))
+
+
   
